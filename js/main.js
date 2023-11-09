8b7ba03d36a385490ed8b810f44b7a96147d171f
kerdaino <kerdaino3@gmail.com>
2023-11-09 05:17:23 -0800

    fourth

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
+// Execute the following code when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.text button');
-    const submitButton = document.querySelector('button[type="submit"]');
-    const ratingSection = document.querySelector('.rating');
-    const thankSection = document.querySelector('.thank');
-    const selectedRatingElement = document.querySelector('.thank h4 span');
-  
-    let selectedRating = 0;
-  
-    buttons.forEach(button => {
-      button.addEventListener('click', function() {
-        selectedRating = parseInt(this.innerText);
-      });
-    });
-  
-    submitButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      if (selectedRating > 0) {
-        // Hide the rating section and show the thank you section
-        ratingSection.classList.add('hide');
-        thankSection.classList.remove('hide');
+  // Get references to various elements
+  const buttons = document.querySelectorAll('.text button'); // Get all buttons in the text section
+  const submitButton = document.querySelector('button[type="submit"]'); // Get the submit button
+  const ratingSection = document.querySelector('.rating'); // Get the rating section
+  const thankSection = document.querySelector('.thank'); // Get the thank you section
+  const selectedRatingElement = document.querySelector('.thank h4 span'); // Get the span element for selected rating
 
-  
-        // Update the selected rating text
-        selectedRatingElement.innerText = selectedRating;
-      }
-    });
+  let selectedRating = 0; // Initialize selectedRating to 0
+
+  // Iterate through each button and add a click event listener
+  buttons.forEach(button => {
+      button.addEventListener('click', function() {
+          selectedRating = parseInt(this.innerText); // Update selectedRating when a button is clicked
+      });
   });
-  
+
+  // Add a click event listener to the submit button
+  submitButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default form submission behavior
+      if (selectedRating > 0) {
+          // If a rating is selected (greater than 0)
+          // Hide the rating section and show the thank you section
+          ratingSection.classList.add('hide'); // Add 'hide' class to rating section
+          thankSection.classList.remove('hide'); // Remove 'hide' class from thank you section
+
+          // Update the selected rating text in the thank you section
+          selectedRatingElement.innerText = selectedRating;
+      }
+  });
+});
